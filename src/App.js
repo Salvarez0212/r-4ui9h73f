4d829f8dabe2e6b1e86ed64a829c21615c342c7b
Salvarez0212 <alvarezsebastian31@gmail.com>
2023-06-09 20:31:22 -0500

    repeater with functional components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component, useState } from "react";
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
-      </div>
-    );
-  }
-}
+const App = () => {
+  const [repeater, setRepeater] = useState("");
+
+  const handleInput = (event) => {
+    setRepeater(event.target.value);
+  };
+  return (
+    <div>
+      <h1>Un repetidor</h1>
+      <input
+        type="text"
+        placeholder="Empieza a escribir algo"
+        onChange={handleInput}
+      />
+      <p className="repeater">{repeater}</p>
+    </div>
+  );
+};
 
 export default App;
